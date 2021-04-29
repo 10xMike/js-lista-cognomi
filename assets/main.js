7426fe2bcc33f29b8b45331c218bae46c4863212
10xMike <michelegec@gmail.com>
2021-04-29 10:29:21 +0200

    Stampa innerHTML

--- assets/main.js
+++ assets/main.js
@@ -8,5 +8,14 @@ console.log(lastnames.push(userLastname));
 // stampa la lista ordinata alfabeticamente
 var lastnamesSorted = lastnames.slice();
 console.log(lastnamesSorted).sort();
+var listElement = document.getElementById('lista').innerHTML;
+
+for (var i = 0; i < lastnamesSorted; i++) {
+    var lastname = lastnamesSorted[i];
+    document.getElementById('lista').innerHTML += listElement + '<li>' + lastname + '</li>';
+}
 // scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
 console.log(lastnamesSorted);
+var position = lastnamesSorted.indexOf(userLastname) + 1;
+console.log(position);
+
