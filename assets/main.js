c3a736d527063305546c2df40f5d3ed903fbb897
10xMike <michelegec@gmail.com>
2021-04-29 10:16:58 +0200

    Soluzione

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,12 @@
 // Lista Cognomi
 // chiedi all’utente il cognome
+var userLastname = prompt('Qual è il tuo cognome?');
 // inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+var lastnames = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
+lastnames.push(userLastname);
+console.log(lastnames.push(userLastname));
 // stampa la lista ordinata alfabeticamente
+var lastnamesSorted = lastnames.slice();
+console.log(lastnamesSorted).sort();
 // scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+console.log(lastnamesSorted);
