2c2eab9c2bc53e25eed6dd23c1d63eda161bdae4
10xMike <michelegec@gmail.com>
2021-05-09 15:42:23 +0200

    Fix del .sort, aggiunta di stampe in console/HTML

--- assets/main.js
+++ assets/main.js
@@ -1,26 +1,19 @@
 // Lista Cognomi
 // chiedi all’utente il cognome
-var userLastname = prompt('Qual è il tuo cognome?');
+var cognomeUtente = prompt('Qual è il tuo cognome?');
 // inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
-var lastnames = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
-lastnames.push(userLastname);
-console.log(lastnames.push(userLastname));
+var listaCognomi = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
+listaCognomi.push(cognomeUtente)
 // stampa la lista ordinata alfabeticamente
-var lastnamesSorted = lastnames.slice();
-lastnamesSorted.sort();
-/* console.log(lastnamesSorted); */
+var sortedArray = listaCognomi.sort();
 
-var listElement = document.getElementById('lista').innerHTML;
-
-for (var i = 0; i < lastnamesSorted; i++) {
-    var lastname = lastnamesSorted[i];
-    /* document.getElementById('lista').innerHTML += listElement + '<li>' + lastname + '</li>'; */
-    document.getElementById('lista').insertAdjacentHTML('beforeend', '<li>' + lastname + '</li>');
-    
+for (var  i = 0; i < sortedArray.length; i++) {
+    var order = sortedArray[i];
+    var ListElement = document.getElementById('list').innerHTML;
 }
-
+console.log(listaCognomi);
 // scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
-console.log(lastnamesSorted);
-var position = lastnamesSorted.indexOf(userLastname) + 1;
-console.log(position);
+
+var posizioneUtente = sortedArray.indexOf(cognomeUtente);
+document.getElementById('surname').innerHTML = "Il tuo cognome si trova in questa posizione: " + (posizioneUtente + 1);
 
